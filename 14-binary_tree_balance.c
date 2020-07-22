ad7ad5d13587746d93bd69bf24d2c1dba516c21d
Emma Navarro <elnavarro55@gmail.com>
2020-07-22 19:58:27 +0000

    Add binary_tree_height function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,10 +11,34 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left)
-		left_height = 1 + binary_tree_balance(tree->left);
+		left_height = 1 + binary_tree_height(tree->left);
 	if (tree->right)
-		right_height = 1 + binary_tree_balance(tree->right);
+		right_height = 1 + binary_tree_height(tree->right);
 	balance_f = left_height - right_height;
 
 	return (balance_f);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: If tree is NULL, your function must return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height_left = 0, height_right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left != NULL)
+		height_left = 1 + binary_tree_height(tree->left);
+
+	if (tree->right != NULL)
+		height_right = 1 + binary_tree_height(tree->right);
+
+	if (height_left >= height_right)
+		return (height_left);
+	else
+		return (height_right);
 }
